28a4f749b13c3bfdd5f46aa1dac40afedcfd3346
Eric Young <ericyoung@gmail.com>
2016-03-15 14:17:20 -0400

    Initial Commit

---
+++ GIFTagFlowLayout/GIFTagFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  GIFTagFlowLayout.h
+//  GIFTagFlowLayout
+//
+//  Created by Eric Young on 3/15/16.
+//  Copyright © 2016 gifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GIFTagFlowLayout.
+FOUNDATION_EXPORT double GIFTagFlowLayoutVersionNumber;
+
+//! Project version string for GIFTagFlowLayout.
+FOUNDATION_EXPORT const unsigned char GIFTagFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIFTagFlowLayout/PublicHeader.h>
+
+
